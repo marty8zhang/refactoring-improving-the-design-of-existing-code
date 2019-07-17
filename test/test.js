f4b1a1890514e5d12206590395c0cece879bee34
Marty Zhang <marty8zhang@gmail.com>
2019-07-17 21:48:37 +1000

    Set up the first unit test case with Mocha.

---
+++ test/test.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import {describe} from "mocha";
+import {strict as assert} from "assert";
+import {Province} from "../chapter_04/Province";
+import {sampleProvinceData} from "../chapter_04/sampleProvinceData";
+
+describe("Province", function() {
+    it("shortfall", function() {
+        const asia = new Province(sampleProvinceData());
+        assert.strictEqual(asia.shortfall, 5);
+    });
+});
