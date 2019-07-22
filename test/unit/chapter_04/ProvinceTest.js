3625ca309186c9777e68f29ca7c69aa306bbdb09
Marty Zhang <marty8zhang@gmail.com>
2019-07-22 18:45:07 +1000

    Use Chai.js as the assertion library.

--- test/unit/chapter_04/ProvinceTest.js
+++ test/unit/chapter_04/ProvinceTest.js
@@ -1,13 +1,19 @@
 "use strict";
 
 import {describe} from "mocha";
-import {strict as assert} from "assert";
+// import {strict as assert} from "assert";
 import {Province} from "../../../chapter_04/Province";
 import {sampleProvinceData} from "../../../chapter_04/sampleProvinceData";
+
+const assert = require('chai').assert;
+const expect = require('chai').expect;
 
 describe("Province", function() {
     it("shortfall", function() {
         const asia = new Province(sampleProvinceData());
+
         assert.strictEqual(asia.shortfall, 5);
+
+        expect(asia.shortfall).to.equal(5);
     });
 });
