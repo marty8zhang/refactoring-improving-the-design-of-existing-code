595da3b776f8d040cb9952a7467df600a7c2d227
Marty Zhang <marty8zhang@gmail.com>
2019-07-23 10:01:34 +1000

    Use nyc as the code coverage monitoring tool.
    Also add a couple of more tests.

--- test/unit/chapter_04/ProvinceTest.js
+++ test/unit/chapter_04/ProvinceTest.js
@@ -5,15 +5,29 @@ import {describe} from "mocha";
 import {Province} from "../../../chapter_04/Province";
 import {sampleProvinceData} from "../../../chapter_04/sampleProvinceData";
 
-const assert = require('chai').assert;
-const expect = require('chai').expect;
+const {assert, expect} = require('chai');
 
 describe("Province", function() {
-    it("shortfall", function() {
-        const asia = new Province(sampleProvinceData());
+    let asia;
 
+    beforeEach(function () {
+        asia = new Province(sampleProvinceData());
+    });
+
+    it("shortfall", function() {
         assert.strictEqual(asia.shortfall, 5);
 
         expect(asia.shortfall).to.equal(5);
+    });
+
+    it('profit', function() {
+        expect(asia.profit).to.equal(230);
+    });
+
+    it('change production', function() {
+        asia.producers[0].production = 20;
+
+        expect(asia.shortfall).equal(-6);
+        expect(asia.profit).equal(292);
     });
 });
