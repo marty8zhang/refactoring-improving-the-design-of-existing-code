dada49ac8e52ccfcc0e607e26441bacad64304a3
Marty Zhang <marty8zhang@gmail.com>
2019-08-13 10:51:04 +1000

    Chapter 06: Use `NumberRange` in `operatingPlan`.

--- test/unit/chapter_06/introduce_parameter_object_test.js
+++ test/unit/chapter_06/introduce_parameter_object_test.js
@@ -1,23 +1,18 @@
 "use strict";
 
 import {describe} from "mocha";
+import {expect} from "chai";
 import {
     station,
     operatingPlan,
     readingsOutsideRange,
-    NumberRange,
 } from "../../../chapter_06/introduce_parameter_object.js";
-import {
-    assert,
-    expect,
-} from "chai";
 
 describe("Chapter 06", function () {
     let actualReadings;
 
     describe("Introduce Parameter Object", function () {
         it("readingsOutsideRange", function () {
-            const range = new NumberRange(operatingPlan.temperatureFloor, operatingPlan.temperatureCeiling);
             const expectedReadings = [
                 {temp: 47, time: '2016-11-10 09:10'},
                 {temp: 58, time: '2016-11-10 09:30'},
@@ -25,7 +20,7 @@ describe("Chapter 06", function () {
 
             actualReadings = readingsOutsideRange(
                 station,
-                range
+                operatingPlan.temperatureRange
             );
 
             expect(actualReadings).to.eql(expectedReadings);
