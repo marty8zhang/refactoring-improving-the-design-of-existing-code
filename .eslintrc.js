ae6ca7d9215c8728406df93661e4db21b3813ba6
Marty Zhang <marty8zhang@gmail.com>
2020-02-14 16:19:15 +1100

    Workaround the `no-unused-expressions` error caused by `chai`'s `expect`.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,23 +2,26 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
   },
   extends: [
     'plugin:vue/essential',
-    'standard'
+    'standard',
   ],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: [
-    'vue'
+    'vue',
+    "chai-friendly",
   ],
   rules: {
+    "no-unused-expressions": 0,
+    "chai-friendly/no-unused-expressions": 2,
   }
 }
